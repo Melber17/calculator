3a365273fcc1f877a5added2066b769915a8e770
Melber17 <melber17@mail.ru>
2021-09-21 10:35:52 +0300

    Add tests for math functions

--- tests/mathFunctions.test.js
+++ tests/mathFunctions.test.js
@@ -1,9 +1,20 @@
 import { describe } from "jest-circus";
-import { multiplication } from "../src/scripts/helpers/mathFunctions";
+import { addition, division, multiplication, radicalExtraction, subtraction } from "../src/scripts/helpers/mathFunctions";
 
 describe("Math function:", () => {
-  test("Function should return multiplication of two numbers", () => {
+  test("should return multiplication of two numbers", () => {
     expect(multiplication(2, 4)).toBe(8);
   });
+  test("should return addition of two numbers", () => {
+    expect(addition(2,4)).toEqual(6)
+  })
+  test("should return subtraction of two numbers", () => {
+    expect(subtraction(4,2)).toEqual(2)
+  })
+  test("should return division of two numbers", () => {
+    expect(division(8,2)).toEqual(4)
+  })
+  test("should return radical expression of two numbers", () => {
+    expect(radicalExtraction(1, 1/10)).toEqual(1)
+  })
 });
-
